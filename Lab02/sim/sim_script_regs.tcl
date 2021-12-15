7a8895eab5ef5d287546d71fb249ffcd0a6df341
Kristina Rogacheva <33569375+kristina-rogacheva@users.noreply.github.com>
2021-12-16 00:15:23 +0100

    SIM: scripts w/ and w/o regs + samples and res txt

---
+++ Lab02/sim/sim_script_regs.tcl
@@ -0,0 +1,23 @@
+# compile the src code
+vcom -93 -work ./work ../src/fpnormalize_fpnormalize.vhd
+vcom -93 -work ./work ../src/fpround_fpround.vhd
+vcom -93 -work ./work ../src/packfp_packfp.vhd
+vcom -93 -work ./work ../src/unpackfp_unpackfp.vhd
+
+vcom -93 -work ./work ../src/reg.vhd
+
+vcom -93 -work ./work ../src/fpmul_stage1_struct.vhd
+vcom -93 -work ./work ../src/fpmul_stage2_struct.vhd
+vcom -93 -work ./work ../src/fpmul_stage3_struct.vhd
+vcom -93 -work ./work ../src/fpmul_stage4_struct.vhd
+vcom -93 -work ./work ../src/fpmul_pipeline.vhd
+
+# compile the tb
+vcom -93 -work ./work ../testbench/data_maker.vhd
+vcom -93 -work ./work ../testbench/fpmul_test.vhd
+
+vsim work.fpmul_test
+# simulate
+add waves *
+run 2000 ns
+
