f4a6d7e4cfd0d88491e2a8f3e19a4e7d5650cab1
AntonioRas <s270259@studenti.polito.it>
2022-02-12 23:36:46 +0100

    Created program with nops in the right position, fixed imm_ext for srai instruction

--- Lab03/RiscV/src/immediate_ext.vhd
+++ Lab03/RiscV/src/immediate_ext.vhd
@@ -21,7 +21,8 @@ begin
 	
 	msb <= (others => instr(31));
 
-	immediate_out <= 	msb(19 downto 0) & instr(31 downto 20)                                                when instr_type_sel = I_INSTR_TYPE else
+	immediate_out <= 	msb(19 downto 0) & msb(6 downto 0) & instr(24 downto 20)							  when (instr_type_sel = I_INSTR_TYPE and instr(14 downto 12) = "101") else 
+						msb(19 downto 0) & instr(31 downto 20)                                                when instr_type_sel = I_INSTR_TYPE else
 				        msb(19 downto 0) & instr(31 downto 25) & instr(11 downto 7)                           when instr_type_sel = S_INSTR_TYPE else
 				        msb(19 downto 0) & instr(31) & instr(7) & instr(30 downto 25) & instr(11 downto 8)    when instr_type_sel = B_INSTR_TYPE else
 				        zeros & instr(31 downto 12)                                                           when instr_type_sel = U_INSTR_TYPE else
